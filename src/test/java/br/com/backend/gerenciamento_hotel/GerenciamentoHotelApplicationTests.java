c9da1e5e749c9d8d4caeb13934ea61032308fcea
gabriellloc <gabrielocdev595@gmail.com>
2026-05-21 17:50:41 -0300

    Subindo as configurações base do projeto

---
+++ src/test/java/br/com/backend/gerenciamento_hotel/GerenciamentoHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.backend.gerenciamento_hotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoHotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
